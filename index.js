ab563fac3c2dd92f3c2230c3bc1961857b3dee68
atikxb <atikxb@gmail.com>
2022-05-06 02:36:55 +0600

    single item delete api added

--- index.js
+++ index.js
@@ -31,56 +31,48 @@ async function run() {
             const email = req.query;
             const items = await itemsCollection.find(email).toArray();
             res.json(items);
-    });
-    // get single item api
-    app.get('/inventory/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const item = await itemsCollection.findOne(query);
-        res.json(item);
-    })
-    // //find single item
-    // app.post('/user', async (req, res) => {
-    //     const query = req.body;
-    //     if (query.id || query.email) {
-    //         const user = await usersCollection.findOne(query);
-    //         res.json(user);
-    //         console.log(user);
-    //     }
+        });
+        // get single item api
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.json(item);
+        })
+        //Inserting item to items collection
+        app.post('/addItem', async (req, res) => {
+            const user = req.body;
+            const result = await itemsCollection.insertOne(user);
+            res.json(result);
+        });
+        //update quantity for single item
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const quantity = req.body;
+            console.log(quantity);
+            // const filter = { _id: ObjectId(id) };
+            // const options = { upsert: true };
+            // const updateDoc = {
+            //     $set: quantity
+            // };
+            // const result = await itemsCollection.updateOne(filter, updateDoc, options);
+            // console.log('updating user', quantity, result);
+            // res.json(result);
+        })
 
-    // });
-    //Inserting item to items collection
-    app.post('/addItem', async (req, res) => {
-        const user = req.body;
-        const result = await itemsCollection.insertOne(user);
-        res.json(result);
-    });
+        //delete api
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemsCollection.deleteOne(query);
+            console.log('deleting service', result);
+            res.json(result);
+        })
 
-
-
-
-
-    // //get single service api
-    // app.get('/services/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = {_id: ObjectId(id)};
-    //     const service = await servicesCollection.findOne(query);
-    //     console.log('getting service', service);
-    //     res.json(service);
-    // })
-    // //delete api
-    // app.delete('/services/:id', async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = {_id: ObjectId(id)};
-    //     const result = await servicesCollection.deleteOne(query);
-    //     console.log('deleting service', result);
-    //     res.json(result);
-    // })
-
-}
+    }
     finally {
-    // await client.close();
-}
+        // await client.close();
+    }
 
 }
 run().catch(console.dir);
