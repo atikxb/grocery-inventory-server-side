87ccdbee4b69631d3bcb7feb410ab79fda408513
atikxb <atikxb@gmail.com>
2022-05-05 18:37:48 +0600

    express js and mongodb connected

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const ObjectId = require('mongodb').ObjectId;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//DB access
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.74f46.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+//default route
+app.get('/', (req, res) => {
+    res.send('Running Inventory server');
+});
+app.listen(port, () => {
+    console.log('running on Inventory server', port);
+});
