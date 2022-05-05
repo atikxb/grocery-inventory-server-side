060f2340761df1306fc92923f02e610d5f415674
atikxb <atikxb@gmail.com>
2022-05-06 04:14:17 +0600

    token api & JWT implemented

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
@@ -9,6 +10,22 @@ const ObjectId = require('mongodb').ObjectId;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//verify token
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];//split token from authHeader
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 //DB access
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.74f46.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -27,10 +44,16 @@ async function run() {
             res.json(items);
         });
         //find current user's items
-        app.get('/useritems', async (req, res) => {
+        app.get('/useritems', verifyJWT, async (req, res) => {//verify token for current user to access this route
+            const decodedEmail = req.decoded.email;//check user token and decoded token match or not
             const email = req.query;
-            const items = await itemsCollection.find(email).toArray();
-            res.json(items);
+            if (email.email === decodedEmail) {
+                const items = await itemsCollection.find(email).toArray();
+                res.json(items);
+            }
+            else {// error if user token and decoded token not match
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
         });
         // get single item api
         app.get('/inventory/:id', async (req, res) => {
@@ -38,7 +61,7 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const item = await itemsCollection.findOne(query);
             res.json(item);
-        })
+        });
         //Inserting item to items collection
         app.post('/addItem', async (req, res) => {
             const user = req.body;
@@ -63,11 +86,17 @@ async function run() {
         //delete api
         app.delete('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await itemsCollection.deleteOne(query);
             console.log('deleting service', result);
             res.json(result);
-        })
+        });
+        // user token api
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.json({ accessToken });
+        });
 
     }
     finally {
